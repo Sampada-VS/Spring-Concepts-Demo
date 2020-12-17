51a76e60d38e18b285bd7df80890b141870603ff
Sampada-VS <samssv1@gmail.com>
2020-12-18 01:07:11 +0530

    Added: logger to log messages using simple logging framework

--- src/main/java/com/example/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/example/springconcepts/SpringConceptsApplication.java
@@ -2,18 +2,19 @@ package com.example.springconcepts;
 
 import com.example.springconcepts.component.DemoBean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
-
+	public static final Logger logger=LoggerFactory.getLogger(SpringConceptsApplication.class);
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Concept Demo.");
+		logger.debug("Welcome to Spring Concept Demo.");
 		ApplicationContext context= SpringApplication.run(SpringConceptsApplication.class, args);
 		DemoBean demoBean=context.getBean(DemoBean.class);
-		System.out.println("Checking context of DemoBean= "+demoBean.toString());
+		logger.debug("Checking context of DemoBean= "+demoBean.toString());
 	}
-
 }
